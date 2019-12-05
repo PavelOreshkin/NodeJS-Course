f0a9af1295a3af84025bd3324cd12f95f4a67398
Pavel Oreshkin <poreskin@axbit.ru>
2019-12-05 23:02:42 +0400

    task1 read csv line by line

--- task2/index.js
+++ task2/index.js
@@ -1,19 +1,23 @@
 import fs from 'fs';
 import csv from 'csvtojson';
+import path from 'path';
 
-const filename = "example.csv";
-const csvFilePath = `${__dirname}/csv/${filename}`;
+const csvFilePath = path.join(__dirname, 'csv', 'example.csv');
+const stream = fs.createReadStream(csvFilePath);
+
+fs.writeFile('./task2/message.txt', '', () => {});
 
 csv()
-  .fromFile(csvFilePath)
-  .then((jsonObj) => {
-    const stream = fs.createWriteStream('./task2/message.txt');
-    jsonObj.forEach(item => {
-      try {
-        stream.write(`${JSON.stringify(item)}\n`);
-      } catch (error) {
-        console.error(error);
-      }
-    })
-    stream.end(null, () => console.log('>>> file is recording'));
-  });
+  .fromStream(stream)
+  .subscribe(jsonObj => {
+    delete jsonObj.Amount;
+    let newObject = {};
+
+    for (let key in jsonObj) {
+      newObject[key.toLowerCase()] = jsonObj[key]
+    }
+    fs.appendFile('./task2/message.txt', `${JSON.stringify(newObject)}\n`, () => {})
+  })
+  .on('error', (err) => {
+    console.error(err)
+  })
